564a74492b5667c42885d53eab32d580fe773254
zqtay <zqtay96@gmail.com>
2023-03-23 03:11:32 -0400

    implemented django application

--- concert/models.py
+++ concert/models.py
@@ -3,15 +3,16 @@ from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
+from django.utils.timezone import now
 
 
 # Create your models here.
 
 class Concert(models.Model):
-    # concert_name
-    # duration
-    # city
-    # date
+    concert_name = models.CharField(max_length=255)
+    duration = models.IntegerField()
+    city = models.CharField(max_length=255)
+    date = models.DateField(default=now)
 
     def __str__(self):
         return self.concert_name
@@ -41,12 +42,12 @@ class ConcertAttending(models.Model):
 
 
 class Photo(models.Model):
-    # id
-    # pic_url
-    # event_country
-    # event_state
-    # event_city
-    # event_date
+    id = models.IntegerField(primary_key=True)
+    pic_url = models.CharField(max_length=1000)
+    event_country = models.CharField(max_length=255)
+    event_state = models.CharField(max_length=255)
+    event_city = models.CharField(max_length=255)
+    event_date = models.DateField(default=now)
 
     class Meta:
         managed = False
@@ -56,9 +57,9 @@ class Photo(models.Model):
 
 
 class Song(models.Model):
-    # id
-    # title
-    # lyrics
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    lyrics = models.TextField()
 
     class Meta:
         managed = False
